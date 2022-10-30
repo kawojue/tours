265dc3de248ea7390a1750a8f6b4249f19e2fb6a
Raheem Kawojue <kawojue08@gmail.com>
2022-10-30 11:29:29 +0100

    setup colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,14 @@ module.exports = {
     extend: {
       fontFamily: {
         'poppins': ['Poppins', 'sans-serif']
+      },
+      colors: {
+        "pry-clr-0": 'rgb(241, 245, 248)',
+        "pry-clr-1": 'rgb(16, 42, 66)',
+        "pry-clr-2": 'rgb(73, 166, 233)',
+        "pry-clr-3": 'rgb(235, 247, 255)',
+        "pry-clr-4": 'rgb(97, 125, 152)',
+        "pry-clr-5": 'rgb(187, 37, 37)'
       }
     },
   },
