660a798836726e97fe9721805a52c8c3263caa12
Raheem Kawojue <kawojue08@gmail.com>
2022-11-01 13:24:54 +0100

    added context hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,9 @@
-import { useState, useEffect } from 'react'
+import { useContext } from 'react'
 import Tours from './components/Tours'
+import Context from './context/DataContext'
 
 function App() {
-  const [tours, setTours] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
-  const [fetchErr, setFetchErr] = useState(null)
-  const [readmore, setReadmore] = useState(true)
-
-  const url = 'https://course-api.com/react-tours-project'
-
-  const fetchTours = async () => {
-    setIsLoading(true)
-    setFetchErr(null)
-    try {
-      const res = await fetch(url)
-      const data = await res.json()
-      setTours(data)
-      setTimeout(() => {
-        setIsLoading(false)
-      }, 1200)
-    } catch {
-      setFetchErr("Please, reload the page.")
-    }
-  }
-
-  useEffect(() => {
-    (async () => await fetchTours())()
-  }, [])
-
-  const notInterested = (id) => {
-    const newTours = tours.filter(tour => tour.id !== id)
-    setTours(newTours)
-  }
+  const { tours, isLoading, fetchErr, fetchTours, notInterested } = useContext(Context)
 
   if (isLoading) {
     return (
