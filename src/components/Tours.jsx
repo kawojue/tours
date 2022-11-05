3647d359cca09af4d3c821f9c0a6549653199296
Raheem Kawojue <kawojue08@gmail.com>
2022-11-05 09:37:07 +0100

    moved state

--- src/components/Tours.jsx
+++ src/components/Tours.jsx
@@ -1,39 +1,36 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import Context from '../context/DataContext'
 
-const Tours = () => {
-    const { tours, notInterested, readmore, setReadmore } = useContext(Context)
+const Tours = ({ id, name, price, image, info }) => {
+    const { notInterested } = useContext(Context)
+    const [readmore, setReadmore] = useState(false)
 
     return (
-        <section className="cards">
-            {tours.map(tour => (
-                <article key={tour.id} className="card">
-                    <img src={tour.image} alt={tour.name}
-                        className="w-full h-[300px] object-cover" />
-                    <div className="py-3 px-5 flex flex-col items-center gap-3 w-full">
-                        <div className="flex justify-between w-full">
-                            <h3 className="text-lg text-pry-clr-1">
-                                {tour.name}
-                            </h3>
-                            <h4 className="tracking-widest bg-pry-clr-3 text-pry-clr-2 px-2 py-0.5">
-                                ${tour.price}
-                            </h4>
-                        </div>
-                        <p>
-                            {`${readmore ? tour.info.slice(0, 189) + '...' : tour.info}`}
-                            <span onClick={() => setReadmore(!readmore)}
-                                className="text-sm text-pry-clr-2 cursor-pointer capitalize">
-                                {` ${readmore ? 'read more.' : 'show less.'}`}
-                            </span>
-                        </p>
-                        <button onClick={() => notInterested(tour.id)}
-                            className="px-5 capitalize py-1 border-2 border-pry-clr-5 text-pry-clr-5 w-fit rounded-md text-sm font-medium tracking-wider">
-                            not interested
-                        </button>
-                    </div>
-                </article>
-            ))}
-        </section>
+        <article className="card">
+            <img src={image} alt={name}
+                className="w-full h-[300px] object-cover" />
+            <div className="py-3 px-5 flex flex-col items-center gap-3 w-full">
+                <div className="flex justify-between w-full">
+                    <h3 className="text-lg text-pry-clr-1">
+                        {name}
+                    </h3>
+                    <h4 className="tracking-widest bg-pry-clr-3 text-pry-clr-2 px-2 py-0.5">
+                        ${price}
+                    </h4>
+                </div>
+                <p>
+                    {`${!readmore ? info.slice(0, 189) + '...' : info}`}
+                    <span onClick={() => setReadmore(!readmore)}
+                        className="text-sm text-pry-clr-2 cursor-pointer capitalize">
+                        {` ${!readmore ? 'read more.' : 'show less.'}`}
+                    </span>
+                </p>
+                <button onClick={() => notInterested(id)}
+                    className="px-5 capitalize py-1 border-2 border-pry-clr-5 text-pry-clr-5 w-fit rounded-md text-sm font-medium tracking-wider">
+                    not interested
+                </button>
+            </div>
+        </article>
     )
 }
 
